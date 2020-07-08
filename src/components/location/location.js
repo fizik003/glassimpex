82e9715bd16fe4f84ab152b19b400544a21209be
fizik003 <kassik0boks@gmail.com>
2020-07-08 10:49:58 +0300

    feat: desktop is ready

---
+++ src/components/location/location.js
@@ -0,0 +1,12 @@
+export function showMap(lat, lng) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZml6aWsiLCJhIjoiY2thdTloZjJhMDk3NTJ5bzlqdmpuZXpzZSJ9.VRtOSj5RqTBn56KIVI1q6g";
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [lat, lng],
+    zoom: 15,
+  });
+
+  const marker = new mapboxgl.Marker().setLngLat([lat, lng]).addTo(map);
+}
---
+++ src/scripts/script.js
@@ -0,0 +1,15 @@
+import { showMap } from "../components/location/location.js";
+// function showMap(lat, lng) {
+//   mapboxgl.accessToken =
+//     "pk.eyJ1IjoiZml6aWsiLCJhIjoiY2thdTloZjJhMDk3NTJ5bzlqdmpuZXpzZSJ9.VRtOSj5RqTBn56KIVI1q6g";
+//   const map = new mapboxgl.Map({
+//     container: "map",
+//     style: "mapbox://styles/mapbox/streets-v11",
+//     center: [lat, lng],
+//     zoom: 15,
+//   });
+
+//   const marker = new mapboxgl.Marker().setLngLat([lat, lng]).addTo(map);
+// }
+
+showMap(24.781375, 59.439932);
