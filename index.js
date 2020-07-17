94b47d7bea631e35dd4725c24653aa0814ce65bb
fizik003 <kassik0boks@gmail.com>
2020-07-17 16:48:47 +0300

    fix: fix style

--- index.js
+++ index.js
@@ -124,7 +124,24 @@ productLinks2.addEventListener("click", (e) => {
 });
 
 // map
+function showMap(lat, lng) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZml6aWsiLCJhIjoiY2thdTloZjJhMDk3NTJ5bzlqdmpuZXpzZSJ9.VRtOSj5RqTBn56KIVI1q6g";
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [lat, lng],
+    zoom: 15,
+    scrollZoom: {
+      ctrl: true,
+    },
+  });
+  map.scrollZoom.disable();
 
+  const marker = new mapboxgl.Marker().setLngLat([lat, lng]).addTo(map);
+}
+
+showMap(24.781375, 59.439932);
 // footer
 
 const btn_openMenu = document.querySelector(".open-menu");
